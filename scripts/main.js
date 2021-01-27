0cbcbe37b3c5b6fb277dfb84ca5c229d964cd187
gundu pooja <S538295@nwmissouri.edu>
2021-01-27 13:46:53 -0600

    updated index.html

--- scripts/main.js
+++ scripts/main.js
@@ -43,6 +43,20 @@ async function locationHandler() {
         }
     });
 
+    function distance(lat1,lon1,lat2,lon2){
+        var R = 6371; // Earth's radius in Km
+        return Math.acos(Math.sin(lat1)*Math.sin(lat2) + 
+                        Math.cos(lat1)*Math.cos(lat2) *
+                        Math.cos(lon2-lon1)) * R;
+      }
+      
+      
+      if (distance(user.lat, user.lon, post.lat, post.lon) <= desiredRadiusInKm){
+        // return true;
+      } else {
+        // return false;
+      }
+
 //     if(error) {
 //         document.getElementById("error-message").innerHTML = "You're not 30 meters nearer to any place.";
 //     } else {
