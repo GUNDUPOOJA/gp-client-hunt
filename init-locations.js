8832c40d8c81dcd188f6fa70d3a8c96395fcb4c0
gundu pooja <S538295@nwmissouri.edu>
2021-01-27 19:02:00 -0600

    modified files

--- init-locations.js
+++ init-locations.js
@@ -1,8 +1,7 @@
 // export default object with the initialization data
 
 export default [
-      
-    {
+          {
         Name: 'Horizons west apartments',
         Latitude: 40.35614, 
         Longitude: -94.88189,
@@ -15,8 +14,8 @@ export default [
      },
            {
         Name: 'mooyah',
-        // Latitude:40.35287,
-        // Longitude:-98.88805,
+         Latitude:40.35287,
+         Longitude:-98.88805,
         North: 40.35287, 
         West: -98.88805,
         South: 40.35287, 
